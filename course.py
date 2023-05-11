a4dec84ee82a1afc6a0a82ca82db5a6f2bab2147
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2023-05-11 18:27:00 +0530

    Added Database MongoDB(PyMongo)

--- course.py
+++ course.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://intern_23:intern%40123@192.168.0.220:2717/interns_b2_23")
+db = client.interns_b2_23
+myDB = db.Chinmoy
+
 app = FastAPI()
 
 class Student(BaseModel):
     name:str
+    roll:int
     course:str
 
-course_details={}   
+course_details={}   #{roll:Student}
 
 @app.get("/")    
 def root():
@@ -14,28 +21,35 @@ def root():
 
 @app.get("/show_details")    #{shows the data of courses and the student enrolled}
 def show():
-    return {"The data for all students ": course_details}
+    result = list(myDB.find({}))
+    final_list = []
+    for stud in result:
+        del stud["_id"]
+        final_list.append(stud)
+    return {"The data for all students ": final_list}
 
-@app.post("/add/{roll}")   
-def add(roll: int, student:Student):
-   if roll in course_details:
-       return {"Error":"Cannot add roll, already enrolled in a course"}
-   else:
-       course_details[roll] = student.dict()
+@app.post("/add")   
+def add(student:Student):
+    if list(myDB.find({"roll":student.roll})) == []:
+       myDB.insert_one(student.dict())
        return {"Success":"Added student in the course successfully"}
+    else:
+       return {"Error":"Cannot add student, already enrolled in a course"}
+       
     
 @app.put("/update/{roll}")
 def update(roll:int,student: Student):
-    if roll not in course_details.keys():
-        return {"Cannot Update":"Student not present in any course."}
+    if list(myDB.find({"roll":roll})) == []:
+        return {"Error":"Student not enrolled in any course"}
     else:
-        course_details[roll] = Student
-        return {"Success":"Updated the student in the course"}
+        myDB.update_one({"roll":roll},{"$set":student.dict()})
+        return {"Success":"Updated succesfully"}
 
 @app.delete("/delete/{roll}")
 def delete(roll:int):
-    if roll not in course_details:
-        return {"Cannot Delete":"Student not present in any course."}
-    else:
-        del course_details[roll]
-        return {"Success":"Deleted the student from the course"}
+    if list(myDB.find({"roll":roll})) == []:
+        return {"Message":"Item not found"}
+    # del course_details["roll"]
+    myDB.delete_one({"roll":roll})
+    return {"Success":"Deleted the student from the course"}
+ 
