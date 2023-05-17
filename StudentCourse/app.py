6bca97f1764bace8452a291dd437eab4eb7e0df9
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2023-05-17 18:53:51 +0530

    Delete StudentCourse directory

--- StudentCourse/app.py
+++
@@ -1,3 +0,0 @@
-import uvicorn
-if __name__ == "__main__":
-    uvicorn.run("course:app")
--- StudentCourse/schema/models.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class Student(BaseModel):
-    name:str
-    roll:int
-    course:str
