52a600f0929bab37e4928cb30abdef9fa8bcaff7
Chinmoy Ranjan Khadgaray <ranjanchinmoy@gmail.com>
2023-05-26 22:41:53 +0530

    committing before turning off the laptop

--- Nav Bar/script.js
+++ Nav Bar/script.js
@@ -1,4 +1,9 @@
 function showDate()
 {
-    
+    <button
+      type="button"
+      onclick="document.getElementById('demo').innerHTML = Date()"
+    >
+      Click me to display Date and Time.
+    </button>;
 }
